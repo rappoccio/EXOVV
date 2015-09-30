9ef601b8f94f48549f798023fa6d1026165afc05
Salvatore Rappoccio <rappoccio@gmail.com>
2015-09-29 22:24:53 -0500

    Adjusting exovv plotter to compare signal region and plot data-over-mc

--- test/plot_exovv.py
+++ test/plot_exovv.py
@@ -22,13 +22,6 @@ parser.add_option('--outname', type='string', action='store',
                   default = "exovvplots",
                   help='Output string for output file')
 
-
-parser.add_option('--dir', type='string', action='store',
-                  dest='dir',
-                  default = "exovvhists",
-                  help='Directory containing root histograms')
-
-
 parser.add_option('--rebin', type='int', action='store',
                   dest='rebin',
                   default = None,
@@ -49,55 +42,8 @@ ROOT.gROOT.Macro("rootlogon.C")
 
 #### Data
 
-f = ROOT.TFile(options.dir + '/' + options.file)
-#trigs = [
-#    'HLT_PFJet140',
-#    'HLT_PFJet200',
-#    'HLT_PFJet260',
-#    'HLT_PFJet320',
-#    'HLT_PFJet400',
-#    'HLT_PFJet450',
-#    'HLT_PFJet500' 
-#    ]
-
-#scales = [
-#    2000, 66, 12, 4, 1, 1, 1
-#    ]
-
-#### MC
-fmcNames = [
-    'exovv_qcd_pt170to300',
-    'exovv_qcd_pt300to470',
-    'exovv_qcd_pt470to600',
-    'exovv_qcd_pt600to800',
-    'exovv_qcd_pt800to1000',
-    'exovv_qcd_pt1000to1400',
-    'exovv_qcd_pt1400to1800',
-    'exovv_qcd_pt1800to2400',
-    'exovv_qcd_pt2400to3200',
-    'exovv_qcd_pt3200toInf',
-]
-
-
-lumi = 40.03 # pb-1
-
-mcscales = [
-    #xs / nevents * lumi
-     117276. / 3468514. * lumi,
-     7823 / 2936644. * lumi,
-     648.2 / 1971800. * lumi,
-     186.9 / 1981608. * lumi,
-     32.293 / 1990208. * lumi,
-     9.4183 / 1487712. * lumi,
-     0.84265 / 197959. * lumi,
-     0.114943 / 194924. * lumi,
-     0.00682981 / 198383. * lumi,
-     0.000165445 / 194528. * lumi
-    ]
-
-fmc = []
-for imc,mcname in enumerate(fmcNames) :
-    fmc.append( ROOT.TFile(options.dir + '/' + mcname + options.mcname + '.root') )
+f = ROOT.TFile( options.file)
+fmc = ROOT.TFile(options.mcname )
     
 
 logy = [ True, False, True, True, True, True, True, True, False, False, False,False,False,False,False,False, ]
@@ -107,35 +53,46 @@ titles = [
     'AK8 p_{T};p_{T} (GeV)',
     'AK8 Rapidity;y',
     'AK8 ungroomed mass;Mass (GeV)',
-    'AK8 soft-drop mass, z_{cut}=0.1, #beta=0;Mass (GeV)',
+     'AK8 soft-drop mass, z_{cut}=0.1, #beta=0;Mass (GeV)',
     'AK8 #tau_{21} = #tau_{2} / #tau_{1};#tau_{21}',
     'AK8 #rho = (m/p_{T}R)^{2};#tau_{21}#rho',
     'Diboson mass;m_{VV} (GeV)'
     ]
 
-mcstacks = []
+mchists = []
 canvs = []
 legs = []
+pads = []
+ratios = []
 
-ROOT.gStyle.SetPadRightMargin(0.15)
+#ROOT.gStyle.SetPadRightMargin(0.15)
+ROOT.gStyle.SetOptStat(000000)
+ROOT.gStyle.SetTitleFont(43)
+#ROOT.gStyle.SetTitleFontSize(0.05)
+ROOT.gStyle.SetTitleFont(43, "XYZ")
+ROOT.gStyle.SetTitleSize(30, "XYZ")
+ROOT.gStyle.SetTitleOffset(3.5, "X")
+ROOT.gStyle.SetLabelFont(43, "XYZ")
+ROOT.gStyle.SetLabelSize(24, "XYZ")
 
 for ihist,histname in enumerate(hists):    
-    mcstack = ROOT.THStack(histname + '_mcstack', titles[ihist])
     canv = ROOT.TCanvas(histname + '_canv', histname +'_canv')
     leg = ROOT.TLegend(0.86, 0.3, 1.0, 0.8)
     leg.SetFillColor(0)
     leg.SetBorderSize(0)
-    for imc,mc in enumerate(fmc) :
-        # I'm an idiot and named the trigger histograms differently from the MC ones
-        if 'nhfAK8' in histname or 'chfAK8' in histname or 'nefAK8' in histname or 'cefAK8' in histname or 'ncAK8' in histname or 'nchAK8' in histname :            
-            hist = mc.Get( 'h_' + histname )
-        else :
-            hist = mc.Get( histname )
-        print 'Getting MC : ' + histname
-        if options.rebin != None :
-            hist.Rebin( options.rebin )
-        hist.Scale( mcscales[imc] )
-        mcstack.Add( hist)
+    mchist = fmc.Get( histname )
+    pad0 = ROOT.TPad( histname + '_pad0', 'pad0', 0.0, 0.3, 1.0, 0.97 )
+    pad1 = ROOT.TPad( histname + '_pad1', 'pad1', 0.0, 0.0, 1.0, 0.3 )
+
+    pad0.SetBottomMargin(0.01)
+    pad1.SetTopMargin(0.01)
+    pad1.SetBottomMargin(0.5)
+    
+    pads.append( [pad0,pad1] )
+    pad0.Draw()
+    pad1.Draw()
+    
+    pad0.cd()
         
 
     s = histname
@@ -143,20 +100,38 @@ for ihist,histname in enumerate(hists):
     hist = f.Get( s )
     hist.SetMarkerStyle(20)
     if options.rebin != None :
+        mchist.Rebin( options.rebin )
         hist.Rebin( options.rebin )
 
     hist.Draw('e')
-    mcstack.GetStack().Last().Draw('hist same')
+    mchist.Draw('hist same')
     hist.Draw('e same')
+    hist.GetXaxis().SetLabelSize(0)
 
     
     if logy[ihist] : 
-        canv.SetLogy()
         hist.SetMinimum(0.1)
+        pad0.SetLogy()
     #leg.Draw()
+
+    pad1.cd()
+    iratio = mchist.Clone()
+    iratio.SetName( 'iratio_' + histname )
+    iratio.Divide( hist )
+    iratio.Draw('e')
+    iratio.SetMinimum(0.0)
+    iratio.SetMaximum(2.0)
+    iratio.UseCurrentStyle()
+    iratio.GetYaxis().SetNdivisions(2,4,0,False)
+    iratio.SetTitle('')
+    iratio.GetXaxis().SetTitle( hist.GetXaxis().GetTitle() )
+    ratios.append(iratio)
+    canv.cd()
     canv.Update()
+    
     canvs.append(canv)
-    mcstacks.append( mcstack )
+    pads.append( [pad0,pad1])
     legs.append(leg)
+    mchists.append( mchist)
     canv.Print( 'exovvplots_' + histname + options.outname + '.png', 'png')
     canv.Print( 'exovvplots_' + histname + options.outname + '.pdf', 'pdf')
