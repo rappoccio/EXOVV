4d7ae686c7b2e53a6a057f8c664a6080e6f829f1
Joseph Pusztay <Josephpu@buffalo.edu>
2016-03-25 12:32:51 -0500

    got the rest of the bins to show in closure test

--- test/2Dplot_mc.py
+++ test/2Dplot_mc.py
@@ -25,7 +25,7 @@ if options.plotUnc:
     jecup = ROOT.TFile('2DClosure_jecup.root')
     jerdn = ROOT.TFile('2DClosure_jerdn.root')
     jerup = ROOT.TFile('2DClosure_jerup.root')
-    for i in range(1, 7):
+    for i in range(0, 7):
         print i 
         jecdna.append(jecdn.Get('mass' + str(i)))
         jecupa.append(jecup.Get('mass' + str(i)))
@@ -43,6 +43,7 @@ ROOT.gStyle.SetLabelSize(25,"XYZ")
 #lumi = 40.
 
 # Variables
+d100 =f.Get('mass0')
 d200 =f.Get('mass1')
 d300 =f.Get('mass2')
 d400 =f.Get('mass3')
@@ -52,7 +53,7 @@ d700 =f.Get('mass6')
 #d800 =d.Get('unfolded_6')
 
 # Canvases
-#uc2 = TCanvas("cdist140", "cdist140")
+uc2 = TCanvas("cdist140", "cdist140")
 uc3 = TCanvas("cdist200", "cdist200")
 uc4 = TCanvas("cdist260", "cdist260")
 uc5 = TCanvas("cdist320", "cdist320")
@@ -60,7 +61,7 @@ uc6 = TCanvas("cdist400", "cdist400")
 uc7 = TCanvas("cdist450", "cdist450")
 uc8 = TCanvas("cdist500", "cdist500")
 
-#uc2.SetLeftMargin(0.15)
+uc2.SetLeftMargin(0.15)
 uc3.SetLeftMargin(0.15)
 uc4.SetLeftMargin(0.15)
 uc5.SetLeftMargin(0.15)
@@ -68,7 +69,7 @@ uc6.SetLeftMargin(0.15)
 uc7.SetLeftMargin(0.15)
 uc8.SetLeftMargin(0.15)
 
-#qtrue140=f.Get("HLT_PFJet140mAK8Gen")
+qtrue100=f.Get('genmass0')
 qtrue200=f.Get("genmass1")
 qtrue260=f.Get("genmass2")
 qtrue320=f.Get("genmass3")
@@ -76,32 +77,32 @@ qtrue400=f.Get("genmass4")
 qtrue450=f.Get("genmass5")
 qtrue500=f.Get("genmass6")
 
-#leg1 = TLegend(.5, .7, .85, .85)
+leg1 = TLegend(.5, .7, .85, .85)
 leg2 = TLegend(.5, .7, .85, .85)
 leg3 = TLegend(.5, .7, .85, .85)
 leg4 = TLegend(.5, .7, .85, .85)
 leg5 = TLegend(.5, .7, .85, .85)
 leg6 = TLegend(.5, .7, .85, .85)
 leg7 = TLegend(.6, .7, .95, .85)
-alegends = [leg2, leg3, leg4, leg5, leg6, leg7]
+alegends = [leg1, leg2, leg3, leg4, leg5, leg6, leg7]
 
-#tlx1 = ROOT.TLatex()
+tlx1 = ROOT.TLatex()
 tlx2 = ROOT.TLatex()
 tlx3 = ROOT.TLatex()
 tlx4 = ROOT.TLatex()
 tlx5 = ROOT.TLatex()
 tlx6 = ROOT.TLatex()
 tlx7 = ROOT.TLatex()
-#tlx1pt = ROOT.TLatex()
+tlx1pt = ROOT.TLatex()
 tlx2pt = ROOT.TLatex()
 tlx3pt = ROOT.TLatex()
 tlx4pt = ROOT.TLatex()
 tlx5pt = ROOT.TLatex()
 tlx6pt = ROOT.TLatex()
 tlx7pt = ROOT.TLatex()
-atlx = [tlx2, tlx3, tlx4, tlx5, tlx6, tlx7]
-atlxpt= [tlx2pt, tlx3pt, tlx4pt, tlx5pt, tlx6pt, tlx7pt]
-datacanvases= [uc3, uc4, uc5, uc6, uc7, uc8]
+atlx = [tlx1, tlx2, tlx3, tlx4, tlx5, tlx6, tlx7]
+atlxpt= [tlx1pt, tlx2pt, tlx3pt, tlx4pt, tlx5pt, tlx6pt, tlx7pt]
+datacanvases= [uc2, uc3, uc4, uc5, uc6, uc7, uc8]
 #pt_bin = {0: '230-320', 1: '320-410', 2: '410-515', 3: '515-610', 4: '610-640', 5: '640-700', 6: '700-Inf'}
 ptbins = ['#bf{p_{T} 200-240 GeV}','#bf{p_{T} 240-310 GeV}','#bf{p_{T} 310-400 GeV}','#bf{p_{T} 400-530 GeV}','#bf{p_{T} 530-650 GeV}','#bf{p_{T} 650-760 GeV}', '#bf{p_{T} >760 GeV}']
 pads = []
@@ -132,8 +133,8 @@ for icanv,canv in enumerate ( datacanvases) :
     pad2.Draw()
     pads.append( [pad1,pad2] )
 
-datalist = [d200, d300, d400, d500, d600, d700]
-MCtruth = [qtrue200, qtrue260, qtrue320, qtrue400, qtrue450, qtrue500]
+datalist = [d100, d200, d300, d400, d500, d600, d700]
+MCtruth = [qtrue100, qtrue200, qtrue260, qtrue320, qtrue400, qtrue450, qtrue500]
 
 
 for i in datacanvases:
@@ -162,10 +163,13 @@ for i in datacanvases:
             err = sqrt(err1*err1 + sys*sys + sys2*sys2)
             hReco.SetBinError(ibin, err)
     ################################## Make top plot nice
-    hReco.SetTitle(";;#frac{1}{#sigma} #frac{d#sigma}{dmdp_{T}} (#frac{1}{GeV^{2}})")
+    hReco.SetTitle(";; #frac{d#sigma}{dmdp_{T}} (#frac{pb}{GeV^{2}})")
     hReco.SetMarkerStyle(20)
     hReco.SetAxisRange( .0001, 1000000, "Y")
-    hReco.Draw("e")
+    hReco.SetFillStyle(1001)
+    hReco.SetFillColor(ROOT.kYellow)
+    hReco.Draw("E2")
+    hReco.Draw("E same")
     MCtruth[index].SetLineColor(2)
     #MCtruth[index].Scale(lumi)
     MCtruth[index].Draw( "hist SAME" )
@@ -173,20 +177,21 @@ for i in datacanvases:
     atlxpt[index].DrawLatex(0.555, 0.559, ptbins[index])
     ################################## legends
     alegends[index].AddEntry(MCtruth[index], 'PYTHIA 8', 'l')
-    alegends[index].AddEntry(hReco, 'Unfolded Reconstructed', 'p')
+    alegends[index].AddEntry(hReco, 'Exp + Stat Uncertainty', 'f')
     alegends[index].Draw()
     #################################### ratio plot stuff
     trueCopy = MCtruth[index].Clone()
     trueCopy.Divide( trueCopy, hReco, 1.0, 1.0, "B" )
     pads[index][1].cd()
-    trueCopy.SetTitle(";Jet Mass (GeV);Ratio")
+    trueCopy.SetTitle(";Jet Mass (GeV);Theory/Data")
     trueCopy.UseCurrentStyle()
     trueCopy.GetXaxis().SetTitleOffset(3)
     trueCopy.SetMinimum(0)
     trueCopy.SetMaximum(2)
-    trueCopy.SetMarkerStyle(20)
     trueCopy.GetYaxis().SetNdivisions(2,4,0,False)
-    trueCopy.Draw("e")
+    trueCopy.SetFillStyle(1001)
+    trueCopy.SetFillColor(ROOT.kYellow)
+    trueCopy.Draw("E2")
     hRatioList.append( trueCopy)
     pads[index][0].Update()
     pads[index][1].Update()
