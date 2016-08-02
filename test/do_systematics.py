4556ad6d614b5d21fd8bc5bdff4b07e807d707f1
Joseph Pusztay <Josephpu@buffalo.edu>
2016-08-02 16:14:47 -0500

    adding systematic variations

---
+++ test/do_systematics.py
@@ -0,0 +1,195 @@
+#from ROOT import *
+import ROOT
+ROOT.gSystem.Load("RooUnfold/libRooUnfold")
+from ROOT import TCanvas, TLegend
+from ROOT import gRandom, TH1, TH1D, cout, RooUnfoldBayes
+from math import sqrt
+from plot_tools import plotter, setup, get_ptbins
+from optparse import OptionParser
+from sysvar import plot_vars, reset
+import pickle
+parser = OptionParser()
+                                 
+(options, args) = parser.parse_args()
+
+f = ROOT.TFile('2DClosure.root')
+parton_shower = ROOT.TFile('PS_hists.root')
+pdfs = ROOT.TFile('unfoldedpdf.root')
+bins = ['p_{T} 200-260 GeV','p_{T} 260-350 GeV','p_{T} 350-460 GeV','p_{T} 460-550 GeV','p_{T} 550-650 GeV','p_{T} 650-760 GeV', 'p_{T} 760-900 GeV', 'p_{T} 900-1000 GeV', 'p_{T} 1000-1100 GeV','p_{T} 1100-1200 GeV',
+    'p_{T} 1200-1300 GeV', 'p_{T} 1300-1400 GeV', 'p_{T} 1400-1500 GeV', 'p_{T} 1500-1600 GeV', 'p_{T} 1600-1700 GeV', 'p_{T} 1700-1800 GeV', 'p_{T} 1800-1900 GeV', 'p_{T} 1900-2000 GeV', 'p_{T} > 2000 GeV']
+scales = [1./60., 1./90., 1./110., 1./90., 1./100., 1./110, 1./140., 1./100., 1./100.,1./100., 1./100., 1./100.,1./100.,1./100.,1./100.,1./100.,1./100.,1./100., 1./10000]
+
+##### WARNING ### WARNING #####<----------------------------------------------
+### This version of pickle  ###<----------------------------------------------
+### is not secure. NEVER use###<----------------------------------------------
+### it to open binary files ###<----------------------------------------------
+### that you did not create ###<----------------------------------------------
+###############################<----------------------------------------------
+RMS_vals = pickle.load(open("ungroomedJackKnifeRMS.p", "rb"))         ########
+RMS_vals_softdrop = pickle.load(open("softdropJackKnifeRMS.p", "rb")) ########
+###############################<----------------------------------------------
+### This version of pickle  ###<----------------------------------------------
+### is not secure. NEVER use###<----------------------------------------------
+### it to open binary files ###<----------------------------------------------
+### that you did not create ###<----------------------------------------------
+##### WARNING ### WARNING #####<----------------------------------------------
+
+jecdna = []
+jecupa = []
+jerdna = []
+jerupa = []
+jernoma = []
+jmrdna = []
+jmrupa = []
+jmrnoma = []
+
+
+jecdnaSD = []
+jecupaSD = []
+jerdnaSD = []
+jerupaSD = []
+jernomaSD = []
+jmrdnaSD = []
+jmrupaSD = []
+jmrnomaSD = []
+
+ps = []
+ps_softdrop = []
+
+ps_differences = []
+ps_differences_softdrop = []
+
+jecdn = ROOT.TFile('2DClosure_jecdn.root')
+jecup = ROOT.TFile('2DClosure_jecup.root')
+jerdn = ROOT.TFile('2DClosure_jerdn.root')
+jerup = ROOT.TFile('2DClosure_jerup.root')
+jernom = ROOT.TFile('2DClosure_jernom.root')
+jmrupfile = ROOT.TFile('2DClosure_jmrup.root')
+jmrdnfile = ROOT.TFile('2DClosure_jmrdn.root')
+jmrnomfile= ROOT.TFile('2DClosure_jmrnom.root')
+
+##################################################################### Get uncertainty hists
+for i in range(0, 19):
+    jecdna.append(jecdn.Get('pythia8_mass' + str(i)))
+    jecupa.append(jecup.Get('pythia8_mass' + str(i)))
+    jerdna.append(jerdn.Get('pythia8_mass' + str(i)))
+    jerupa.append(jerup.Get('pythia8_mass' + str(i)))
+    jernoma.append(jernom.Get('pythia8_mass'+str(i)))
+    jmrupa.append(jmrupfile.Get('pythia8_mass' + str(i)))
+    jmrdna.append(jmrdnfile.Get('pythia8_mass' + str(i)))
+    jmrnoma.append(jmrnomfile.Get('pythia8_mass' + str(i)))
+
+    jecdnaSD.append(jecdn.Get('pythia8_massSD' + str(i)))
+    jecupaSD.append(jecup.Get('pythia8_massSD' + str(i)))
+    jerdnaSD.append(jerdn.Get('pythia8_massSD' + str(i)))
+    jerupaSD.append(jerup.Get('pythia8_massSD' + str(i)))
+    jernomaSD.append(jernom.Get('pythia8_massSD'+str(i)))
+    jmrupaSD.append(jmrupfile.Get('pythia8_massSD' + str(i)))
+    jmrdnaSD.append(jmrdnfile.Get('pythia8_massSD' + str(i)))
+    jmrnomaSD.append(jmrnomfile.Get('pythia8_massSD' + str(i)))
+
+ROOT.gStyle.SetOptStat(000000)
+ROOT.gStyle.SetTitleFont(43,"XYZ")
+ROOT.gStyle.SetTitleSize(30,"XYZ")
+ROOT.gStyle.SetTitleOffset(1.0, "XY")
+ROOT.gStyle.SetLabelFont(43,"XYZ")
+ROOT.gStyle.SetLabelSize(25,"XYZ")
+datacanvasesSD = []
+datacanvases= []
+
+################################################################################# generate canvases 
+for x in range(0, 19):
+    datacanvases.append(TCanvas("cdist"+str(x), "cdist"+str(x)))
+    datacanvasesSD.append(TCanvas("cdist" + str(x) + "SD", "cdist"+str(x)+"SD"))
+
+######################################################################################### Get Central Value hists and generate legends etc
+
+datalist = []
+datalistSD = []
+MCtruth = []
+MCtruthSD = []
+atlx = []
+atlxpt = []
+alegends = []
+alegendsSD = []
+atlxSD = []
+atlxSDpt = []
+comparisons = []
+for x in range(0, 19):
+    datalistSD.append(f.Get('pythia8_massSD'+str(x)))
+    datalist.append(f.Get('pythia8_mass'+str(x)))
+    MCtruth.append(f.Get('genmass' + str(x)))
+    MCtruthSD.append(f.Get('genmassSD' + str(x)))
+    atlx.append(ROOT.TLatex())
+    atlxpt.append(ROOT.TLatex())
+    atlxSD.append(ROOT.TLatex())
+    atlxSDpt.append(ROOT.TLatex())
+    alegends.append(TLegend(.3, .65, .5, .88))
+    alegendsSD.append(TLegend(.3, .65, .5, .88))
+    datacanvases[x].SetLeftMargin(0.15)
+    datacanvasesSD[x].SetLeftMargin(0.15)
+################################################################################################################# Get Parton Showering Unc.
+ps_differences = []
+ps_differences_softdrop = []
+for i in range(0, 19):
+    temp_diff = []
+    temp_softdrop_diff = []
+    ps.append(parton_shower.Get('pythia8_unfolded_by_herwig'+str(i)))
+    ps_softdrop.append(parton_shower.Get('pythia8_unfolded_by_herwig_softdrop'+str(i)))
+    temp_unc = (ps[i] - datalist[i])
+    temp_softdrop_unc = (ps_softdrop[i] - datalistSD[i])
+    temp_unc.Scale(scales[i])
+    temp_softdrop_unc.Scale(scales[i])
+    for ibin in xrange(1,temp_unc.GetNbinsX()):
+        temp_diff.append(abs(temp_unc.GetBinContent(ibin)))
+        temp_softdrop_diff.append(abs(temp_softdrop_unc.GetBinContent(ibin)))
+    ps_differences.append(temp_diff)
+    ps_differences_softdrop.append(temp_softdrop_diff)
+
+
+############################################################################## PDF differences and PDF differences comparisons 
+pdf_differences = []
+pdf_differences_softdrop = []
+
+pdf_up = []
+pdf_dn = []
+
+pdf_upsd = []
+pdf_dnsd = []
+comparisons_softdrop = []
+
+complegends = []
+complegendssd = []
+
+for i in range(0, 19):
+    temp_pdf_diff = []
+    temp_softdrop_pdf_diff = []
+    pdf_up.append(pdfs.Get('pdf_up'+str(i)))
+    pdf_dn.append(pdfs.Get('pdf_dn'+str(i)))
+    pdf_upsd.append(pdfs.Get('pdf_up_softdrop'+str(i)))
+    pdf_dnsd.append(pdfs.Get('pdf_dn_softdrop'+str(i)))
+     
+    temp_unc = (pdf_up[i] - pdf_dn[i])
+    temp_unc_softdrop = (pdf_upsd[i] - pdf_dnsd[i])
+    temp_unc.Scale(scales[i])
+    temp_unc_softdrop.Scale(scales[i])
+    for ibin in xrange(1, temp_unc.GetNbinsX()):
+        temp_pdf_diff.append(abs(temp_unc.GetBinContent(ibin)))
+        temp_softdrop_pdf_diff.append(abs(temp_unc_softdrop.GetBinContent(ibin)))
+    pdf_differences.append(temp_pdf_diff)
+    pdf_differences_softdrop.append(temp_softdrop_pdf_diff)
+
+# Canvases
+ptbins = ['#{p_{T} 200-260 GeV}','#{p_{T} 260-350 GeV}','#{p_{T} 350-460 GeV}','#{p_{T} 460-550 GeV}','#{p_{T} 550-650 GeV}','#{p_{T} 650-760 GeV}', '#{p_{T} >760 GeV}']
+
+for leg in alegends :
+    leg.SetFillColor(0)
+    leg.SetBorderSize(0)
+for leg in alegendsSD :
+    leg.SetFillColor(0)
+    leg.SetBorderSize(0)
+
+histstokeep = []
+
+plot_vars(datacanvases, datalist, jecupa, jecdna, jerupa, jerdna, jernoma, ps_differences, pdf_differences, alegends, "mcvariations_", jmrupa, jmrdna, jmrnoma, bins, keephists=histstokeep, jackknifeRMS=RMS_vals)
+plot_vars(datacanvasesSD, datalistSD, jecupaSD, jecdnaSD, jerupaSD, jerdnaSD, jernomaSD, ps_differences_softdrop, pdf_differences_softdrop, alegendsSD, "mcvariations_softdrop_", jmrupaSD, jmrdnaSD, jmrnomaSD, bins, softdrop="MMDT Beta=0", keephists=histstokeep, jackknifeRMS=RMS_vals_softdrop, histname="Soft Drop ")
---
+++ test/sysvar.py
@@ -0,0 +1,223 @@
+import ROOT
+ROOT.gSystem.Load("RooUnfold/libRooUnfold")
+from ROOT import TCanvas, TLegend
+from ROOT import gRandom, TH1, TH1D, cout
+from math import sqrt
+
+def plot_vars(canvas_list, data_list, jecup_list, jecdn_list, jerup_list, jerdn_list, jernom_list, psdif_list, pdfdif_list, legends_list, outname_str, jmrup_list, jmrdn_list, jmrnom_list, ptbins_dict, softdrop= "", keephists=[], jackknifeRMS=False, histname = "Ungroomed "):
+    scales = [1./60., 1./90., 1./110., 1./90., 1./100., 1./110, 1./140., 1./100., 1./100.,1./100., 1./100., 1./100.,1./100.,1./100.,1./100.,1./100.,1./100.,1./100., 1./10000]
+    mbinwidths = [1., 4., 5, 10., 20, 20., 20., 20., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50., 50.]
+
+    for i, canv in enumerate(canvas_list):
+        data_list[i].UseCurrentStyle()
+        data_list[i].Scale(scales[i])
+        ########################################################################################## Get JER and JES Hists
+        hRMS = data_list[i]
+        nom = jernom_list[i]
+        jesUP  = jecup_list[i]
+        jeOWN = jecdn_list[i]
+        jerUP  = jerup_list[i]
+        jerDOWN = jerdn_list[i]
+        ########################################################################################## Get JMR hists
+        jmrup = jmrup_list[i]
+        jmrdn = jmrdn_list[i]
+        jmrnom = jmrnom_list[i]
+        ########################################################################################## Scale the hists for Pt bins
+        jmrup.Scale(scales[i])
+        jmrdn.Scale(scales[i])
+        jmrnom.Scale(scales[i])
+        jesUP.Scale(scales[i])
+        jeOWN.Scale(scales[i])
+        jerUP.Scale(scales[i])
+        jerDOWN.Scale(scales[i])
+        nom.Scale(scales[i])
+        for ibin in xrange(1, hRMS.GetNbinsX()):
+            hRMS.SetBinContent(ibin, hRMS.GetBinContent(ibin) * 1. / mbinwidths[ibin-1])
+            hRMS.SetBinError(ibin, hRMS.GetBinError(ibin) * 1. / mbinwidths[ibin-1])
+            hRMS.SetBinError(ibin, hRMS.GetBinError(ibin) + ((jackknifeRMS[i][ibin-1])*scales[i]*(1./mbinwidths[ibin-1]) ) )
+        hReco = hRMS.Clone()
+        reset(hReco)
+        ########################################################################################## Scale the hists for mass bins
+        for ibin in xrange(1, hReco.GetNbinsX()):
+            jmrup.SetBinContent(ibin, jmrup.GetBinContent(ibin) * 1./mbinwidths[ibin-1])
+            jmrdn.SetBinContent(ibin, jmrdn.GetBinContent(ibin) * 1./mbinwidths[ibin-1])
+            jmrnom.SetBinContent(ibin, jmrnom.GetBinContent(ibin) * 1./mbinwidths[ibin-1])
+
+            jesUP.SetBinContent(ibin, jesUP.GetBinContent(ibin) * 1./mbinwidths[ibin-1])
+            jeOWN.SetBinContent(ibin, jeOWN.GetBinContent(ibin) * 1./mbinwidths[ibin-1])
+            jerUP.SetBinContent(ibin, jerUP.GetBinContent(ibin) * 1./mbinwidths[ibin-1])
+            jerDOWN.SetBinContent(ibin, jerDOWN.GetBinContent(ibin) * 1./mbinwidths[ibin-1])
+            nom.SetBinContent(ibin, nom.GetBinContent(ibin) * 1./mbinwidths[ibin-1])
+        ########################################################################################## Get JER and JES Uncertainties
+        for ibin in xrange(1,hReco.GetNbinsX()):
+            val = float(hReco.GetBinContent(ibin))
+            err1 = float(hReco.GetBinError(ibin))
+            upjes = float(abs(jesUP.GetBinContent(ibin) - nom.GetBinContent(ibin)))
+            downjes = float(abs(nom.GetBinContent(ibin) - jeOWN.GetBinContent(ibin)))
+            sys = float(((upjes + downjes)/2.))
+            upjer = float(abs(jerUP.GetBinContent(ibin) - nom.GetBinContent(ibin)))
+            downjer = float(abs(nom.GetBinContent(ibin) - jerDOWN.GetBinContent(ibin)))
+            sys2 = float(((upjer + downjer )/2.))
+            err = sqrt(sys*sys + sys2*sys2) + err1  
+            hReco.SetBinError(ibin, err)
+        ####################################################################################### Get Jet Mass Resolution Band
+        hRecoJMR = hReco.Clone()
+        reset(hRecoJMR)
+        for ibin in xrange(1, hRecoJMR.GetNbinsX()):
+            val = float(hRecoJMR.GetBinContent(ibin))
+            err1 = float(hRecoJMR.GetBinError(ibin))
+            upjmr = float(abs(jmrup.GetBinContent(ibin) - jmrnom.GetBinContent(ibin)))
+            downjmr = float(abs(jmrnom.GetBinContent(ibin) - jmrdn.GetBinContent(ibin)))
+            sys = float(((upjmr + downjmr)/2.))
+            err = err1 + sys
+            hRecoJMR.SetBinError(ibin, err)
+        ######################################################################################## Get Parton Shower Uncertainties
+        hRecoCopy = hRecoJMR.Clone()
+        reset(hRecoCopy)
+        for ibin in xrange(1, hRecoCopy.GetNbinsX()):
+            hRecoCopy.SetBinError(ibin, (psdif_list[i][ibin-1] * 1./ mbinwidths[ibin-1]) )
+        ######################################################################################## Get PDF Uncertainties
+        hRecoPDF = hRecoJMR.Clone()
+        reset(hRecoPDF)
+        for ibin in xrange(1, hRecoPDF.GetNbinsX()):
+            hRecoPDF.SetBinError(ibin, (pdfdif_list[i][ibin-1] * 1./ mbinwidths[ibin-1] ) )
+
+        ##################################################################################### divide error by bin content and set to unity
+        for ibin in xrange(1,hRMS.GetNbinsX()):
+            if hRMS.GetBinContent(ibin) > 0: 
+                hRMS.SetBinError(ibin, hRMS.GetBinError(ibin)/hRMS.GetBinContent(ibin))
+                hReco.SetBinError(ibin, hReco.GetBinError(ibin)/hReco.GetBinContent(ibin))
+                hRecoCopy.SetBinError(ibin, hRecoCopy.GetBinError(ibin)/hRecoCopy.GetBinContent(ibin))
+                hRecoJMR.SetBinError(ibin, hRecoJMR.GetBinError(ibin)/hRecoJMR.GetBinContent(ibin))
+                hRecoPDF.SetBinError(ibin, hRecoPDF.GetBinError(ibin)/hRecoPDF.GetBinContent(ibin))
+            else:
+                hRMS.SetBinError(ibin, 0)
+                hReco.SetBinError(ibin, 0)
+                hRecoCopy.SetBinError(ibin, 0)
+                hRecoJMR.SetBinError(ibin, 0)
+                hRecoPDF.SetBinError(ibin, 0)
+            hRMS.SetBinContent(ibin, 1.0)
+            hReco.SetBinContent(ibin, 1.0)
+            hRecoCopy.SetBinContent(ibin, 1.0)
+            hRecoJMR.SetBinContent(ibin, 1.0)
+            hRecoPDF.SetBinContent(ibin, 1.0)
+
+        ######################################################################## Clone em all
+        hRMSup = hRMS.Clone()
+        hRMSdn = hRMS.Clone()
+        hRecoup = hReco.Clone()
+        hRecodn = hReco.Clone()
+        hRecoCopyup = hRecoCopy.Clone()
+        hRecoCopydn = hRecoCopy.Clone()
+        hRecoJMRup = hRecoJMR.Clone()
+        hRecoJMRdn = hRecoJMR.Clone()
+        hRecoPDFup = hRecoPDF.Clone()
+        hRecoPDFdn = hRecoPDF.Clone()
+
+        for ibin in xrange(1, hRMS.GetNbinsX()):
+            hRMSup.SetBinContent(ibin, 1 + (hRMS.GetBinError(ibin) / 2.0) )
+            hRMSdn.SetBinContent(ibin, 1 - (hRMS.GetBinError(ibin) / 2.0) )
+            hRecoup.SetBinContent(ibin, 1 + (hReco.GetBinError(ibin) / 2.0) )
+            hRecodn.SetBinContent(ibin, 1 - (hReco.GetBinError(ibin) / 2.0) )
+            hRecoCopyup.SetBinContent(ibin, 1 + (hRecoCopy.GetBinError(ibin) / 2.0) )
+            hRecoCopydn.SetBinContent(ibin, 1 - (hRecoCopy.GetBinError(ibin) / 2.0) )
+            hRecoJMRup.SetBinContent(ibin, 1 + (hRecoJMR.GetBinError(ibin) / 2.0) )
+            hRecoJMRdn.SetBinContent(ibin, 1 - (hRecoJMR.GetBinError(ibin) / 2.0) )
+            hRecoPDFup.SetBinContent(ibin, 1 + (hRecoPDF.GetBinError(ibin) / 2.0) )
+            hRecoPDFdn.SetBinContent(ibin, 1 - (hRecoPDF.GetBinError(ibin) / 2.0) )
+
+        ######################################################################## Format, Draw, and save
+
+        hRMSup.SetLineStyle(2)
+        hRMSdn.SetLineStyle(2)
+        hRMSup.SetLineColor(1)
+        hRMSdn.SetLineWidth(2)
+        hRMSdn.SetLineColor(1)
+        hRMSup.SetLineWidth(2)
+        hRMSup.GetXaxis().SetTitleSize(30)
+        hRMSup.GetXaxis().SetTitleOffset(.72)
+        hRMSup.GetYaxis().SetLabelSize(10)
+
+
+        hRecoup.SetLineStyle(3)
+        hRecodn.SetLineStyle(3)
+        hRecoup.SetLineColor(2)
+        hRecodn.SetLineColor(2)
+        hRecoup.SetLineWidth(2)
+        hRecodn.SetLineWidth(2)
+
+        hRecoCopyup.SetLineStyle(4)
+        hRecoCopydn.SetLineStyle(4)
+        hRecoCopyup.SetLineColor(3)
+        hRecoCopydn.SetLineColor(3)
+        hRecoCopyup.SetLineWidth(2)
+        hRecoCopydn.SetLineWidth(2)
+
+        hRecoJMRup.SetLineStyle(5)
+        hRecoJMRdn.SetLineStyle(5)
+        hRecoJMRup.SetLineColor(4)
+        hRecoJMRdn.SetLineColor(4)
+        hRecoJMRdn.SetLineWidth(2)
+        hRecoJMRup.SetLineWidth(2)
+
+        hRecoPDFup.SetLineStyle(6)
+        hRecoPDFdn.SetLineStyle(6)
+        hRecoPDFup.SetLineColor(6)
+        hRecoPDFdn.SetLineColor(6)
+        hRecoPDFup.SetLineWidth(2)
+        hRecoPDFdn.SetLineWidth(2)
+
+        canvas_list[i].cd()
+        hRMSup.GetYaxis().SetTitle("Variation")
+        hRMSup.GetYaxis().SetTitleSize(30)
+        hRMSup.GetYaxis().SetLabelSize(30)
+        hRMSup.SetTitle( histname + "Uncertainty Variations Unfolded for " +ptbins_dict[i] )
+        hRMSup.SetTitleSize(30)
+        hRMSup.GetXaxis().SetTitle("Jet Mass (GeV)")
+        hRMSup.SetMinimum(0.0)
+        hRMSup.SetMaximum(2.0)
+        if i == 18:
+            hRMSup.SetAxisRange(1, 1900,"X")
+        elif i >= 15 and i < 18:
+            hRMSup.SetAxisRange(1, 1000, "X")
+        elif i >= 11 and i < 15:
+            hRMSup.SetAxisRange(1, 900, "X")
+        elif i >= 9 and i < 11:
+            hRMSup.SetAxisRange(1, 800, "X")
+        elif i >= 7 and i < 9:
+            hRMSup.SetAxisRange(1, 700, "X")
+        elif i >= 6 and i < 7:
+            hRMSup.SetAxisRange(1, 600, "X")
+        elif i >= 5 and i < 6:
+            hRMSup.SetAxisRange(1, 500, "X")
+        elif i >= 3 and i < 5:
+            hRMSup.SetAxisRange(1, 400, "X")
+        elif i >= 2 and i < 3 :
+            hRMSup.SetAxisRange(1, 300,"X")
+        elif i < 2:
+            hRMSup.SetAxisRange(1, 200,"X")
+        hRMSup.Draw('hist')
+        hRMSdn.Draw('hist same')
+        hRecoup.Draw('hist same')
+        hRecodn.Draw('hist same')
+        hRecoCopyup.Draw('hist same')
+        hRecoCopydn.Draw('hist same')
+        hRecoJMRup.Draw('hist same')
+        hRecoJMRdn.Draw('hist same')
+        hRecoPDFup.Draw('hist same')
+        hRecoPDFdn.Draw('hist same')
+        ####################################################################################### Legends Filled
+        legends_list[i].AddEntry(hRecoJMRup, 'JMR', 'l')
+        legends_list[i].AddEntry(hRecoPDFup, 'PDF', 'l')
+        legends_list[i].AddEntry(hRecoCopyup, 'Parton Shower', 'l')
+        legends_list[i].AddEntry(hRecoup, 'JES+JER', 'l')
+        legends_list[i].AddEntry(hRMSup, 'Stat', 'l')
+        legends_list[i].Draw()
+
+        canvas_list[i].Draw()
+        canvas_list[i].SaveAs(outname_str + str(i) + ".png")
+        canvas_list[i].SaveAs(outname_str + str(i) + ".pdf")
+
+def reset(histogram):
+    for ibin in xrange(1, histogram.GetNbinsX()):
+        histogram.SetBinError(ibin, 0)
