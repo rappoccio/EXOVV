69fe8183fcc26227aaed010bb95f025e31f26411
Salvatore Rappoccio <rappoccio@gmail.com>
2016-10-02 13:11:18 -0500

    Adding CRAB files to rerun MC with correct PDF treatment

---
+++ test/execute_for_crab_mc_76x.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export PATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/lhapdf6/6.1.5-cms/bin/:$PATH
+export LD_LIBRARY_PATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/lhapdf6/6.1.5-cms/lib/:$LD_LIBRARY_PATH
+export PYTHONPATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/lhapdf6/6.1.5-cms/lib/python2.7/site-packages/:$PYTHONPATH
+
+python execute_for_crab.py --makeResponseMatrix2D --isMC --doPDFs --xrootd root://cmsxrootd.fnal.gov/ --applyTriggers --puStr CHS --metStr Full --btagger CSVv2 
