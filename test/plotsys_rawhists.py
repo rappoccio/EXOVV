0bfc1db691acd0390a9de9d55dfe018210363848
Salvatore Rappoccio <rappoccio@gmail.com>
2016-10-18 14:13:28 -0400

    Adding per-pt-bin raw plots plus uncertainties

--- test/plotsys_rawhists.py
+++ test/plotsys_rawhists.py
@@ -37,7 +37,8 @@ ROOT.gStyle.SetTitleFont(43)
 #ROOT.gStyle.SetTitleFontSize(0.05)
 ROOT.gStyle.SetTitleFont(43, "XYZ")
 ROOT.gStyle.SetTitleSize(30, "XYZ")
-#ROOT.gStyle.SetTitleOffset(3.5, "X")
+ROOT.gStyle.SetTitleOffset(3.5, "X")
+ROOT.gStyle.SetTitleOffset(1.5, "Y")
 ROOT.gStyle.SetLabelFont(43, "XYZ")
 ROOT.gStyle.SetLabelSize(24, "XYZ")
 
@@ -59,7 +60,7 @@ def getProjsY( name, h, norm=True ) :
     return projs
 
 
-def setup1(canv):
+def setupPads(canv, pads):
     canv.cd()
     pad1 = ROOT.TPad('pad' + canv.GetName() + '1', 'pad' + canv.GetName() + '1', 0., 0.3, 1.0, 1.0)
     pad1.SetBottomMargin(0)
@@ -70,7 +71,8 @@ def setup1(canv):
     pad2.SetBottomMargin(0.5)
     pad1.Draw()
     pad2.Draw()
-    return [pad1,pad2]
+    pads.append( [pad1,pad2] )
+    return [pad1, pad2]
 
 
 datahists = []
@@ -112,17 +114,33 @@ for isyst in xrange( len( systs ) ) :
 canvs = []
 stacks = []
 allpads = []
+copies = []
+ratios = []
+tlx = ROOT.TLatex()
+tlx.SetNDC()
+tlx.SetTextFont(43)
+tlx.SetTextSize(25)
+legs = []
 
 for iptbin in xrange( len(dataprojs) ) :
-    c = ROOT.TCanvas("c" + str(iptbin), "c" + str(iptbin), 600, 800)
-    pads = setup1(c)
+    c = ROOT.TCanvas("c" + str(iptbin), "c" + str(iptbin), 800, 600)
+    pads = setupPads(c, allpads)
     pads[0].cd()
-    #c.SetLogx()
-    canvs.append(c)
+    pads[0].SetLogx()
+    axisrange = []
+    if iptbin == 11:
+        axisrange = [1,2000]
+    elif iptbin > 7 and iptbin < 11:
+        axisrange = [1,900]
+    elif iptbin > 3 and iptbin < 8:
+        axisrange = [1,600]
+    elif iptbin < 4:
+        axisrange = [1,400]
+
+
     for imbin in xrange( 1,dataprojs[iptbin].GetNbinsX()+1 ) :
         val = pyprojs[iptbin].GetBinContent( imbin )
-        errstat = pyprojs[iptbin].GetBinError ( imbin )
-        errtot = errstat**2
+
 
         jecup = pysysprojs[JECUP][iptbin].GetBinContent( imbin )
         jecdn = pysysprojs[JECDN][iptbin].GetBinContent( imbin )
@@ -132,38 +150,77 @@ for iptbin in xrange( len(dataprojs) ) :
         jmrdn = pysysprojs[JMRDN][iptbin].GetBinContent( imbin )
         pdfup = pysysprojs[PDFUP][iptbin].GetBinContent( imbin )
         pdfdn = pysysprojs[PDFDN][iptbin].GetBinContent( imbin )
-        print ' pt,m = %6d,%6d : jec=(%8.3e-%8.3e), jer=(%8.3e - %8.3e), jmr=(%8.3e - %8.3e), pdf=(%8.3e - %8.3e)' % ( iptbin, imbin, jecup, jecdn, jerup, jerdn, jmrup, jmrdn, pdfup, pdfdn )
-        if abs(val) > 0.001: 
+        hwval = hwprojs[iptbin].GetBinContent(imbin)
+        
+        #print ' pt,m = %6d,%6d : jec=(%8.3e-%8.3e), jer=(%8.3e - %8.3e), jmr=(%8.3e - %8.3e), pdf=(%8.3e - %8.3e)' % ( iptbin, imbin, jecup, jecdn, jerup, jerdn, jmrup, jmrdn, pdfup, pdfdn )
+        errtot = 0.
+        if abs(val) > 0.001:
+            errstat = pyprojs[iptbin].GetBinError ( imbin ) / val
+            errtot = errstat**2 
             jec = (jecup - jecdn) * 0.5 / val
             jer = (jerup - jerdn) * 0.5 / val
             jmr = (jmrup - jmrdn) * 0.5 / val
             pdf = (pdfup - pdfdn) * 0.5 / val
-            errtot += jec**2 + jer**2 + jmr**2 + pdf**2
-        errtot = sqrt(errtot) * val
+            psunc = (hwval - val) * 0.5 / val
+            errtot += jec**2 + jer**2 + jmr**2 + pdf**2 + psunc**2
+            errtot = sqrt(errtot) * val
         pyprojs[iptbin].SetBinError( imbin, errtot )
-    pyprojs[iptbin].SetFillStyle(3004)
-    pyprojs[iptbin].SetFillColor(ROOT.kRed)
+    pyprojs[iptbin].SetFillStyle(3001)
+    pyprojs[iptbin].SetFillColor(ROOT.kBlue)
     pyprojs[iptbin].SetMarkerStyle(0)
+    pyprojs[iptbin].SetMarkerSize(0)
     dataprojs[iptbin].SetMarkerStyle(20)
-    stack = ROOT.THStack( )
-    stack.Add( dataprojs[iptbin], "e" )
+    stack = ROOT.THStack( "stack" + str(iptbin), ";;Fraction / GeV" )
     stack.Add( pyprojs[iptbin], "e2" )
+    stack.Add( dataprojs[iptbin], "e" )
 
     #pyprojs[iptbin].Draw("e2 same")
     #pyprojs[iptbin].GetXaxis().SetRangeUser(1,1300)
     #dataprojs[iptbin].Draw("e same")
     stack.Draw("nostack")
+    maxval = stack.GetMaximum()
+    stack.SetMaximum( maxval * 0.6 )
+    stack.GetXaxis().SetRangeUser(axisrange[0],axisrange[1])
     stacks.append(stack)
+    tlx.DrawLatex(0.2, 0.926, "CMS Preliminary")
+    tlx.DrawLatex(0.72, 0.926, "2.3 fb^{-1} (13 TeV)")
 
+    if 'AK8SD' not in options.hist : 
+        leg = ROOT.TLegend(0.3, 0.6, 0.55, 0.85)
+    elif 'AK8SD' in options.hist :
+        leg = ROOT.TLegend(0.6, 0.6, 0.85, 0.85)
+    leg.SetFillColor(0)
+    leg.SetBorderSize(0)
+    leg.AddEntry( dataprojs[iptbin], 'Data', 'p')
+    leg.AddEntry( pyprojs[iptbin], 'PYTHIA8 MC', 'f')
+    leg.Draw()
+    legs.append(leg)
+    
     pads[1].cd()
-    frac = pyprojs[iptbin].Clone("frac" + pyprojs[iptbin].GetName() )
-    frac.Divide( dataprojs[iptbin] )
-    frac.Draw("e2")
-    frac.GetYaxis().SetRangeUser(0,2)
-     
+    pads[1].SetLogx()
+    ratio = pyprojs[iptbin].Clone("ratio" + pyprojs[iptbin].GetName() )
+    ratio.UseCurrentStyle()
+    ratio.SetTitle(";Mass (GeV);MC / Data")
+    ratio.Divide( dataprojs[iptbin] )
+    ratio.SetFillColor(ROOT.kBlue)
+    ratio.SetFillStyle(3001)
+    ratio.GetYaxis().SetNdivisions(2,4,0,False)
+    ratio.GetXaxis().SetRangeUser(axisrange[0],axisrange[1])
+    ratio.GetYaxis().SetRangeUser(0,2)
+    ratio.SetMarkerStyle(0)
+    ratio.SetMarkerSize(0)
+    ratio.Draw("e6")
+    ratios.append(ratio)
+    pads[0].Update()
+    pads[1].Update()
+    c.Update()
+    canvs.append(c)
 
-    allpads.append(pads)
-
-
+    if 'AK8SD' not in options.hist : 
+        c.Print("jetplots_h_m_meas" + str(iptbin) + "jetht.png", "png")
+        c.Print("jetplots_h_m_meas" + str(iptbin) + "jetht.pdf", "pdf")
+    elif 'AK8SD' in options.hist : 
+        c.Print("jetplots_h_msd_meas" + str(iptbin) + "jetht.png", "png")
+        c.Print("jetplots_h_msd_meas" + str(iptbin) + "jetht.pdf", "pdf")
 
  
