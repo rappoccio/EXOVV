7e22dd053e37f98229c11528baff1e0faa324160
Salvatore Rappoccio <rappoccio@gmail.com>
2017-07-26 14:37:47 -0400

    Making a JMS plot

--- test/plot_scales_3d.py
+++ test/plot_scales_3d.py
@@ -30,6 +30,9 @@ f = ROOT.TFile(options.infile)
 ptbinstrs = ['#bf{200 < p_{T} < 260 GeV}','#bf{260 < p_{T} < 350 GeV}','#bf{350 < p_{T} < 460 GeV}','#bf{460 < p_{T} < 550 GeV}','#bf{550 < p_{T} < 650 GeV}','#bf{650 < p_{T} < 760 GeV}', '#bf{760 < p_{T} < 900 GeV}', '#bf{900 < p_{T} < 1000 GeV}', '#bf{1000 < p_{T} < 1100 GeV}','#bf{1100 < p_{T} < 1200 GeV}',
     '#bf{1200 < p_{T} < 1300 GeV}', '#bf{p_{T} > 1300 GeV}']
 
+ptquickstrs = ['200 GeV','260 GeV','350 GeV','460 GeV','550 GeV','650 GeV', '760 GeV', '900 GeV', '1000 GeV','1100 GeV', '1200 GeV', '1300 GeV']
+
+    
 histstrs = [
     'h3_mreco_mgen',
     'h3_mreco_mgen_softdrop_nomnom',
@@ -39,6 +42,8 @@ histstrs = [
 hists = []
 canvs = []
 fits = []
+multigraphs = []
+legs = []
 ROOT.gStyle.SetOptStat(000000)
 ROOT.gStyle.SetPalette(ROOT.kBlackBody)
 ROOT.gStyle.SetTitleOffset( 1.0, "Y" )
@@ -53,16 +58,30 @@ tlx.SetNDC()
 tlx.SetTextFont(43)
 tlx.SetTextSize(25)
 
+markers = [20,21,22,23,29,33,34,24,25,26,27,28]
+styles = [1,2,3,1,2,3,1,2,3,1,2,3]
+colors = [1,1,1,2,2,2,103,103,103,4,4,4]
+
 lines = []
 graphs = []
 
 import array
 
+
 for ihist in xrange( len(histstrs) ):
 
     htemp = f.Get(histstrs[ihist])
 
-
+    totresc = ROOT.TCanvas("totresc_" +str(ihist), "totresc_" + str(ihist) )
+    mg = ROOT.TMultiGraph()
+    canvs.append(totresc)
+    multigraphs.append(mg)
+    leg = ROOT.TLegend(0.16, 0.64, 0.84, 0.84)
+    leg.SetHeader("p_{T} Bins")
+    leg.SetNColumns(4)
+    leg.SetFillColor(0)
+    leg.SetBorderSize(0)
+    legs.append(leg)
 
     for ptbin in xrange( 1, htemp.GetNbinsX() ) :
 
@@ -104,7 +123,7 @@ for ihist in xrange( len(histstrs) ):
                 graphX.append( hist2D.GetXaxis().GetBinLowEdge(mbin) )
                 graphDX.append( hist2D.GetXaxis().GetBinWidth(mbin) / 2.0 )
                 graphY.append( fit.GetParameter(1) )
-                graphDY.append( fit.GetParError(1) )
+                graphDY.append( fit.GetParameter(2) )
                 proj.SetMaximum(1.3 * proj.GetMaximum())
                 if ihist == 0 : 
                     cm.Print("mreco_mgen_fits_pt_" + str(ptbin) + "_m_" + str(mbin) + "_ungroomed.png", "png")
@@ -115,19 +134,28 @@ for ihist in xrange( len(histstrs) ):
 
 
         resc = ROOT.TCanvas("resc_" +str(ihist) + "_" + str(ptbin), "resc_" + str(ptbin) )
-        massres = ROOT.TGraphErrors( len(graphX), graphX, graphY, graphDX, graphDY )
+        #massres = ROOT.TGraphErrors( len(graphX), graphX, graphY, graphDX, graphDY )
+        massres = ROOT.TGraph( len(graphX), graphX, graphY )
         massres.SetName("massres_" + str(ptbin))
-        massres.SetTitle("p_{T} = " + ptbinstrs[ptbin] +"-" + ptbinstrs[ptbin] + ";Jet mass (GeV);Fitted m_{reco}/m_{gen}")
-        massres.SetFillColor(ROOT.kBlue)
-        massres.SetLineColor(ROOT.kBlue)
-        massres.SetLineWidth(3)
-        massres.SetFillStyle(3005)
-        massres.Draw("AL3")        
+        massres.SetTitle(";Jet mass (GeV);Fitted m_{reco}/m_{gen}")
+        leg.AddEntry( massres, ptquickstrs[ptbin-1], "l" )
+        #massres.SetFillColor(colors[ptbin])
+        massres.SetLineColor(colors[ptbin-1])
+        massres.SetLineWidth(2)
+        massres.SetLineStyle(styles[ptbin-1])
+        #massres.SetFillStyle(3005)
+        resc.cd()
+        massres.Draw("AL3")
         resc.SetLogx()
         massres.SetMaximum(2.0)
         massres.SetMinimum(0.0)
         graphs.append(massres)
         canvs.append(resc)
+        prelim.Draw()
+        tlx.DrawLatex ( 0.6, 0.830, ptbinstrs[ptbin])
+        prelim.DrawLatex( 0.2, 0.926, "CMS Simulation" )
+        prelim.DrawLatex( 0.62, 0.926, "2.3 fb^{-1} (13 TeV)")
+        mg.Add( massres )
 
         if ihist == 0 : 
             resc.Print("mreco_mgen_pt_" + str(ptbin) +"_ungroomed.png", "png")
@@ -135,7 +163,6 @@ for ihist in xrange( len(histstrs) ):
         else :
             resc.Print("mreco_mgen_pt_" + str(ptbin) +"_groomed.png", "png")
             resc.Print("mreco_mgen_pt_" + str(ptbin) +"_groomed.pdf", "pdf")
-
 
 
         resc2 = ROOT.TCanvas("resc2_" +str(ihist) + "_" + str(ptbin), "resc2_" + str(ptbin) )
@@ -172,3 +199,22 @@ for ihist in xrange( len(histstrs) ):
         #c.SetLogx()
         #canvs.append(c)        
         #hists.append(hist2D)
+
+    totresc.cd()
+    mg.Draw("AL")
+    mg.SetTitle(";Groomed jet mass (GeV);JMS")
+    mg.SetMinimum(0.8)
+    mg.SetMaximum(1.2)
+    mg.GetXaxis().SetRangeUser(10., 1000.)
+    totresc.SetLogx()
+    #tlx.DrawLatex ( 0.6, 0.830, ptbinstrs[ptbin])
+    prelim.DrawLatex( 0.2, 0.926, "CMS Simulation" )
+    prelim.DrawLatex( 0.62, 0.926, "2.3 fb^{-1} (13 TeV)")
+    leg.Draw()
+    
+    if ihist == 0 : 
+        totresc.Print("mreco_mgen_pt_ungroomed.png", "png")
+        totresc.Print("mreco_mgen_pt_ungroomed.pdf", "pdf")
+    else :
+        totresc.Print("mreco_mgen_pt_groomed.png", "png")
+        totresc.Print("mreco_mgen_pt_groomed.pdf", "pdf")
